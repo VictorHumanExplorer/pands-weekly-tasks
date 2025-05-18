7e70fc73ea66e2362a5503c968b526bfd80ad0bd
Victor Pajares Liberal <vpajaresl@hotmail.com>
2025-05-18 20:30:56 +0100

    week02

---
+++ week02/bank.py
@@ -0,0 +1,6 @@
+#bank.py
+#Author: Victor Pajares Liberal
+print("$ python bank.py")
+Amount1 = int(input("Enter amount1 (in cent): "))
+Amount2 = int(input("Enter amount2 (in cent): "))
+print("The sum of these is €" + str(float(Amount1 + Amount2)*0.01))
